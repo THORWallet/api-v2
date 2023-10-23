5534cb2c6e07c63e56e0a5acf4abe3b7887f537d
Csjernyik Janos <janos.csjernyik@gmail.com>
2023-10-23 17:53:56 +0200

    Config app port from .env

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, options)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(3050)
+  await app.listen(process.env.PORT)
 }
 bootstrap()
