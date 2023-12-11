076b746d6afc0591978817f48a756f2612e18779
Csjernyik Janos <janos.csjernyik@gmail.com>
2023-12-11 15:43:30 +0100

    Fix

--- src/modules/gas/gas.controller.ts
+++ src/modules/gas/gas.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/commo
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { EthGasResponse, GetGasDto } from './entities/get-gas.dto'
 import { EthereumGasService } from './services/ethereum.gas.service'
+import { Chain } from '../../constants'
 
 @Controller('gas')
 @ApiTags('Gas')
@@ -16,7 +17,7 @@ export class GasController {
   @ApiResponse({ status: 200, description: 'Success', type: EthGasResponse })
   async gas(@Body() getGasDto: GetGasDto): Promise<EthGasResponse> {
     switch (getGasDto.asset.chain) {
-      case 'ETH':
+      case Chain.Ethereum:
         return this.ethGasService.getGasFee(getGasDto)
       default:
         throw new HttpException(`${getGasDto.asset.chain} is not supported.`, HttpStatus.BAD_REQUEST)
